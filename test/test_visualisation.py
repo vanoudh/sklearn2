2b5ced5fe23e16935b0bfee776b93ad16afb5959
vanoudheusden <marc.vanoudheusden@gmail.com>
2018-01-05 11:40:30 +0100

    fix portability

--- test/test_visualisation.py
+++ test/test_visualisation.py
@@ -5,6 +5,7 @@ Created on Mon Mar 27 16:10:21 2017
 @author: mvanoudh
 """
 
+import os
 import matplotlib.pyplot as plt  #analysis:ignore
 import numpy as np
 import pandas as pd
@@ -18,13 +19,13 @@ pd.options.display.width = 160
 
 
 def test_heatmap():
-    import os
     print(os.getcwd())
     res1 = pd.DataFrame(np.random.rand(5, 5))
     fig = plt.figure(figsize=(8,6))      
     show_heatmap(res1)
     plt.xlabel('line => column')
-    fig.savefig('test_out\\heatmap.png')
+    os.makedirs('test_out', exist_ok=True)
+    fig.savefig(os.path.join('test_out', 'heatmap.png'))
 
     
 if __name__ == "__main__":
