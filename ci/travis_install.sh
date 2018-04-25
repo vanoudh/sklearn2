224e3dfcc2690ebea9b887b25291ed48426d2993
marc <marc.vanoudheusden@gmail.com>
2018-04-25 22:42:21 +0200

    nosetests

--- ci/travis_install.sh
+++ ci/travis_install.sh
@@ -14,7 +14,7 @@ conda info -a
 #conda create -q -n test-environment python=$TRAVIS_PYTHON_VERSION
 #source activate test-environment
 
-conda install pandas
+conda install scikit-learn matplotlib pandas
 pip install "."
 
 
--- ci/travis_test.sh
+++ ci/travis_test.sh
@@ -4,3 +4,4 @@ set -e
 
 python --version
 python -c "import pandas; print('pandas %s' % pandas.__version__)"
+nosetests -v
